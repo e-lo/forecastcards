f39e6e5c46ad20e593f4d4566814ecb7badfbd22
Elizabeth Sall <easall@gmail.com>
2018-11-26 14:24:42 -0800

    re-organize
    move to standard package format; add requirements.txt

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='ForecastCards',
+    version='0.1.0dev',
+    packages=['forecastcards'],
+    install_requires=['pandas==0.22',
+                      'requests',
+                      'goodtables',
+                      'tableschema',
+                      'graphviz',
+                      'statsmodels'],
+    package_data={'forecastcards': ['examples'/*]},
+    long_description=open('README.txt').read(),
+)
