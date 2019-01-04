83cb9d17af95804fd5c2bb3a55eff46248e1ea53
Elizabeth Sall <easall@gmail.com>
2019-01-03 16:11:14 -0800

    Test and Document using multiple datasource
    Fixes #4

--- tests/test_cardset.py
+++ tests/test_cardset.py
@@ -14,16 +14,33 @@ schema_locs = { 'poi':'https://raw.github.com/e-lo/forecastcards/master/spec/en/
                 "observations": "https://raw.github.com/e-lo/forecastcards/master/spec/en/observations-schema.json",
                 "forecast": "https://raw.github.com/e-lo/forecastcards/master/spec/en/forecast-schema.json",
 }
+
+valid_project_control = set(['lu123','rx123'])
+
 @pytest.mark.master
 @pytest.mark.basic
 def test_local_cardset():
     #forecastcards.Cardset(data_loc = ex_data)
-    forecastcards.Cardset()
+    cardset = forecastcards.Cardset()
+    assert(valid_project_control == set(cardset.validated_projects))
 
 @pytest.mark.master
 @pytest.mark.basic
 def test_github_cardset():
-    forecastcards.Cardset(data_loc = gh_data)
+    cardset = forecastcards.Cardset(data_loc = gh_data)
+    assert(valid_project_control == set(cardset.validated_projects))
+
+@pytest.mark.master
+def test_github_add_local_cardset():
+    cardset = forecastcards.Cardset(data_loc = gh_data, exclude_projects=['lu123'])
+    cardset.add_projects(data_loc=ex_data, select_projects=['lu123'])
+    assert(valid_project_control == set(cardset.validated_projects))
+
+@pytest.mark.master
+def test_local_add_github_cardset():
+    cardset = forecastcards.Cardset(data_loc = ex_data, exclude_projects=['lu123'])
+    cardset.add_projects(data_loc=gh_data, select_projects=['lu123'])
+    assert(valid_project_control == set(cardset.validated_projects))
 
 if __name__ == '__main__':
 
@@ -31,3 +48,7 @@ if __name__ == '__main__':
     test_local_cardset()
     print ("test github")
     test_github_cardset()
+    print ("test github add local")
+    test_github_add_local_cardset()
+    print ("test local add github")
+    test_local_add_github_cardset()
